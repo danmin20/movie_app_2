ee3457b9e41fece5ff52c7633f01f1c8752b9f3b
Lee Jeong Min <danmin20@khu.ac.kr>
2020-01-05 15:11:06 +0900

    Updates

---
+++ precache-manifest.8a1bc7882f4e01b274379c88f3ead39b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad9d11d8cd4540ad213a02071ec8d655",
+    "url": "/movie_app_2/index.html"
+  },
+  {
+    "revision": "7c09002c0a8026d85c3d",
+    "url": "/movie_app_2/static/css/main.28b750cd.chunk.css"
+  },
+  {
+    "revision": "73be264925e22aecaad5",
+    "url": "/movie_app_2/static/js/2.036ea6cc.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/movie_app_2/static/js/2.036ea6cc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7c09002c0a8026d85c3d",
+    "url": "/movie_app_2/static/js/main.f38fdfc9.chunk.js"
+  },
+  {
+    "revision": "1fdf926eec9f8ac3a200",
+    "url": "/movie_app_2/static/js/runtime-main.423d73ff.js"
+  }
+]);
