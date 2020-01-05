27cdf918b1954410f0f2566f700728c8dad904d1
Lee Jeong Min <danmin20@khu.ac.kr>
2020-01-05 20:59:06 +0900

    Updates

--- precache-manifest.944661e6f8848da536250c5a3dc88e8a.js
+++ precache-manifest.944661e6f8848da536250c5a3dc88e8a.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5189bdcdb3c1cf02a9cf833721c9767a",
+    "revision": "cd82818ea2738e44d733263eba25d99b",
     "url": "/movie_app_2/index.html"
   },
   {
-    "revision": "93862db4ca70b2403215",
-    "url": "/movie_app_2/static/css/main.78b3a349.chunk.css"
+    "revision": "104c4397100663900c3c",
+    "url": "/movie_app_2/static/css/main.dfa120ed.chunk.css"
   },
   {
     "revision": "9af6d2ba1caad3972136",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/movie_app_2/static/js/2.c9f0f0a7.chunk.js.LICENSE"
   },
   {
-    "revision": "93862db4ca70b2403215",
-    "url": "/movie_app_2/static/js/main.b3c3271c.chunk.js"
+    "revision": "104c4397100663900c3c",
+    "url": "/movie_app_2/static/js/main.33d0ea34.chunk.js"
   },
   {
     "revision": "1fdf926eec9f8ac3a200",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_app_2/precache-manifest.4c391ffe89578c1fb64ac8dbb6e3ffbd.js"
+  "/movie_app_2/precache-manifest.944661e6f8848da536250c5a3dc88e8a.js"
 );
 
 self.addEventListener('message', (event) => {
